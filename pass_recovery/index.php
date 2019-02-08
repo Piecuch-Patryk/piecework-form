c9bac2e298a347721a3257449280ed004dd331b7
Piecuch Patryk <33668858+Piecuch-Patryk@users.noreply.github.com>
2019-02-08 20:32:52 +0000

    Fixed placeholder toggle.
    When the input has some value label become visible.

--- pass_recovery/index.php
+++ pass_recovery/index.php
@@ -78,6 +78,11 @@ if(!isset($_SESSION['newpass'])){
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script>
 		// Toggle placeholder;
+		if(!$('#login').is(':invalid')){
+			$('#login').next().css('display', 'block').animate({
+				opacity: 1
+			});
+		}
 		$('.input').on('focus', function(){
 			const text = this.placeholder;
 			// show label;
