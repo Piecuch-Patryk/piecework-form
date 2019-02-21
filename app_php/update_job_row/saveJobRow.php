4d3ee2014266c5082e0904b5c29635f668ded071
Piecuch Patryk <33668858+Piecuch-Patryk@users.noreply.github.com>
2019-02-21 22:32:23 +0000

    Added new feature.
    Save every job row in database.

---
+++ app_php/update_job_row/saveJobRow.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST['invoice'])){
+	session_start();
+	require_once '../connection/db-conn-users.php';
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	try{
+		$connection = new mysqli($host, $db_user, $db_password, $db_name);
+		if($connection->connect_errno != 0){
+			throw new exception(mysqli_connect_error());
+		}else{
+			$stmt = $connection->prepare("INSERT INTO `jobs`(`id`, `userId`, `date`, `invoice`, `rangeType`, `size`, `price`, `base`, `guttering`, `waterbutt`, `coritec`, `shelves`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			$stmt->bind_param('isississsss', $_SESSION['userId'], $_POST['date'], $_POST['invoice'], $_POST['range'], $_POST['size'], $_POST['price'], $_POST['base'], $_POST['guttering'], $_POST['waterbutt'], $_POST['coritec'], $_POST['shelves']);
+			$stmt->execute();
+		}
+	}catch(Exception $e){
+		die('Error: ' . $e);
+	}
+}else {
+	header('Location: ../../private/');
+}
+?>
